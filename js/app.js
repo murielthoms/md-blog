1d3a72529988489da022fec96be72b7a82be25eb
muriel <murielthoms@gmail.com>
2016-10-25 16:26:11 +0200

    étape 2

--- js/app.js
+++ js/app.js
@@ -1,9 +1,55 @@
 (function(){
 	"use strict";
 	var app = {
+
+		article: null,
+
 		init:function(){
-			// let's go
+
+			app.listeners();
+			this.appelAlice();
+			this.json();
 		},
+
+		listeners:function(){
+			$('#btn').on('click', this.appelAlice.bind(this));
+			$('#').on('click', this.json.bind(this));
+		},
+			
+
+		appelAlice:function(){
+			var url = "http://192.168.1.40:1337/alice.md";
+			$.ajax(url).done(this.ajaxDone);
+		},
+
+		ajaxDone: function(data){
+			app.listeners();
+			this.article = data;
+			var alice = new showdown.Converter(),
+			text = this.article,
+			html = alice.makeHtml(text);
+			console.log(html);
+
+			$('#md').html(html);
+		},
+
+		json: function(){
+			var url = "http://192.168.1.40:1337/menu.json";
+			$.ajax(url).done(this.ajxDone);
+
+		},
+		ajxDone: function(data){
+			app.listeners();
+			for(var i = 0; i < data.menu.length; i++){
+				var title = data.menu[i].title;
+				$('#titre').append('<button id="btn'+i+'">'+ title +'</button>');
+			
+				}
+		},
+
+
+
+
 	};
 
 
